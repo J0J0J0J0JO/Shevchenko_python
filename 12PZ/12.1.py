f486a6d3f45a2a35f701415e9389762962486a2b
tralalela trallala <nikitasevcenko110@gmail.com>
2025-06-06 22:54:11 +0300

    Add files via upload

---
+++ 12PZ/12.1.py
@@ -0,0 +1,19 @@
+# І. Организовать и вывести последовательость из 20 целых чисел, 
+# выбрать не повторяющиеся элементы, найти их количество. Элементы больше 5 увеличить в два раза.
+import random
+
+# Генерация последовательности из 20 целых чисел
+sequence = [random.randint(1, 10) for _ in range(20)]
+print("Исходная последовательность:", sequence)
+
+# Выбор неповторяющихся элементов
+unique_elements = list(set(sequence))
+print("Неповторяющиеся элементы:", unique_elements)
+
+# Подсчет количества уникальных элементов
+count_unique = len(unique_elements)
+print("Количество уникальных элементов:", count_unique)
+
+# Увеличение элементов больше 5 в два раза
+processed_elements = [x * 2 if x > 5 else x for x in unique_elements]
+print("Обработанные элементы (больше 5 увеличено в 2 раза):", processed_elements)
